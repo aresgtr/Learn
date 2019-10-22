b4ca689422bfb35349a450821fd4939749a48baa
aresgtr <qi@jsonar.com>
2019-10-22 10:10:05 -0700

    Array Challenge

---
+++ JAVA/ArrayChallenge/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] myIntegers = getIntegers(5);
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        
+    }
+}
