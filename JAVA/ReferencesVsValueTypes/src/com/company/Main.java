602c6bf5b69e6387e1270c202ddec9867ef3ec3f
aresgtr <784359442@qq.com>
2019-10-22 10:58:26 -0700

    Reference Types vs Value Types

--- JAVA/ReferencesVsValueTypes/src/com/company/Main.java
+++ JAVA/ReferencesVsValueTypes/src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        int myIntValue = 10;
+        int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherIntArray = " + Arrays.toString(anotherArray));
+
+        anotherArray[0] = 1;
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherIntArray = " + Arrays.toString(anotherArray));
+
+        modifyArray(myIntArray);
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherIntArray = " + Arrays.toString(anotherArray));
+    }
+
+    private static void modifyArray(int[] array) {
+        array[0] = 2;
     }
 }
