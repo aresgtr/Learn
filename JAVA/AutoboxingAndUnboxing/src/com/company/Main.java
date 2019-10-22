cb4e6efa302900018383b354bd5923543c064e54
aresgtr <784359442@qq.com>
2019-10-22 16:34:29 -0700

    Autoboxing and Unboxing

--- JAVA/AutoboxingAndUnboxing/src/com/company/Main.java
+++ JAVA/AutoboxingAndUnboxing/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.ArrayList;
 
+
+// Wrapper
 class IntClass {
     private int myValue;
 
@@ -31,6 +33,30 @@ public class Main {
         ArrayList<IntClass> intClassArrayList = new ArrayList<>();
         intClassArrayList.add(new IntClass(54));
 
+        Integer integer = new Integer(54);
+        Double doubleValue = new Double(12.25);
+
+        ArrayList<Integer> intArrayList = new ArrayList<>();
+        for(int i = 0; i <= 10; i++) {
+            intArrayList.add(Integer.valueOf(i));
+        }
+
+        for(int i = 0; i <= 10; i++) {
+            System.out.println(i + " --> " + intArrayList.get(i).intValue());
+        }
+
+
+        Integer myIntValue = 56;
+
+        ArrayList<Double> myDoubleValues = new ArrayList<>();
+        for(double dbl = 0.0; dbl <= 10.0; dbl += 0.5) {
+            myDoubleValues.add(Double.valueOf(dbl));
+        }
+
+        for (int i = 0; i < myDoubleValues.size(); i++) {
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " --> " + value);
+        }
 
     }
 }
