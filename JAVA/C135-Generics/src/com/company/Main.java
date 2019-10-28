62ef4be2709a95a92aa64b4f4265db78950fefea
aresgtr <784359442@qq.com>
2019-10-28 11:19:24 -0700

    Generics Part One Introduction

---
+++ JAVA/C135-Generics/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayList items = new ArrayList();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+        printDoubled(items);
+    }
+
+    private static void printDoubled(ArrayList n) {
+        for (Object i:n) {
+            System.out.println((Integer) i * 2);
+        }
+    }
+}
