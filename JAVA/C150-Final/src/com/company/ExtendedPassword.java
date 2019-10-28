731f1dcd94d6dd8f82af56df9bcb5252fcb6a7de
aresgtr <784359442@qq.com>
2019-10-28 16:31:45 -0700

    Final Part 2 and Static Initializers

---
+++ JAVA/C150-Final/src/com/company/ExtendedPassword.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ExtendedPassword extends Password {
+    private int decryptedPassword;
+
+    public ExtendedPassword(int password) {
+        super(password);
+        this.decryptedPassword = password;
+    }
+
+}
