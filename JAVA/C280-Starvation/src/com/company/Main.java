456edf4e09c40ed79b94936cb4ad40c9edeb92ba
aresgtr <784359442@qq.com>
2019-10-31 14:27:55 -0700

    Thread Starvation

---
+++ JAVA/C280-Starvation/src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    private static Object lock = new Object();
+
+    public static void main(String[] args) {
+	// write your code here
+        Thread t1 = new Thread(new Worder(ThreadColor.ANSI_RED), "Priority 10");
+        Thread t2 = new Thread(new Worder(ThreadColor.ANSI_BLUE), "Priority 8");
+        Thread t3 = new Thread(new Worder(ThreadColor.ANSI_GREEN), "Priority 6");
+        Thread t4 = new Thread(new Worder(ThreadColor.ANSI_CYAN), "Priority 4");
+        Thread t5 = new Thread(new Worder(ThreadColor.ANSI_PURPLE), "Priority 2");
+
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+    }
+
+
+
+
+
+
+
+
+
+
+    private static class Worder implements Runnable {
+        private int runCount = 1;
+        private String threadColor;
+
+        public Worder(String threadColor) {
+            this.threadColor = threadColor;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                synchronized (lock) {
+                    System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
+                    //  execute critical section of code
+                }
+            }
+        }
+    }
+}
