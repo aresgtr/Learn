491c5bdd1eff1fae4e5a40d044f6c29969aa468f
aresgtr <784359442@qq.com>
2019-10-31 14:11:38 -0700

    More on Deadlocks

--- JAVA/C279-SayHello/src/com/company/Main.java
+++ JAVA/C279-SayHello/src/com/company/Main.java
@@ -7,26 +7,38 @@ public class Main {
         PolitePerson jane = new PolitePerson("Jane");
         PolitePerson john = new PolitePerson("John");
 
-        jane.sayHello(john);
-
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                jane.sayHello(john);
-//            }
-//        }).start();
+//        jane.sayHello(john);
 //
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                john.sayHello(jane);
-//            }
-//        }).start();
+//        System.out.println("=================================");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                jane.sayHello(john);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                john.sayHello(jane);
+            }
+        }).start();
     }
 
 
-
-
+    /*
+        1.  Thread1 acquires the lock on the jane object and enters the sayHello() method.
+            It prints to the console, then suspends.
+        2.  Thread2 acquires the lock on the john object and enters the sayHello() method.
+            It prints to the console, then suspends.
+        3.  Thread1 runs again and wants to say hello back to the john object. It tries to call the sayHelloBack() method
+            using the john object that ws passed into the sayHello() method,
+            but Thread2 is holding the john lock, so Thread1 suspends.
+        4.  Thread2 runs again and wants to say hello back to the jane object. It tries to call the sayHelloBack() method
+            using the jane object that ws passed into the sayHello() method,
+            but Thread1 is holding the jane lock, so Thread1 suspends.
+     */
 
 
 
