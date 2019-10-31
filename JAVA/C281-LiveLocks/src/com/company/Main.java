58696c4effc1659c2ee32151f8e14f62ce7ab095
aresgtr <784359442@qq.com>
2019-10-31 15:04:17 -0700

    Live Locks

---
+++ JAVA/C281-LiveLocks/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        final Worker worker1 = new Worker("Worker 1", true);
+        final Worker worker2 = new Worker("Worker 2", true);
+
+    }
+}
