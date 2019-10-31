7af44a59eb2af9fb49cb8460de4b79bd6fbc4bc0
aresgtr <784359442@qq.com>
2019-10-31 12:05:34 -0700

    Thread Pools

--- JAVA/C272-ProducerConsumer/src/com/company/Main.java
+++ JAVA/C272-ProducerConsumer/src/com/company/Main.java
@@ -2,6 +2,11 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -13,12 +18,37 @@ public class Main {
         List<String> buffer = new ArrayList<>();
         ReentrantLock bufferLock = new ReentrantLock();
 
+        //  Executive Service
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+//        new Thread(producer).start();
+//        new Thread(consumer1).start();
+//        new Thread(consumer2).start();
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2); //  does not shutdown automatically
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_WHITE + "I'm being printed for the Callable Class");
+                return "This is the callable result";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            System.out.println("Thread running the task was interrupted");
+        } catch (ExecutionException e) {
+            System.out.println("Something went wrong");
+        }
+
+        executorService.shutdown();
     }
 }
