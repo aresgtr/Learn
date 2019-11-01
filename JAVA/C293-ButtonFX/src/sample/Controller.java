eac18d9560259f4c48b69f3a31ccd2ef2f146680
aresgtr <784359442@qq.com>
2019-11-01 15:25:57 -0700

    JavaFX does not work for now

--- JAVA/C293-ButtonFX/src/sample/Controller.java
+++ JAVA/C293-ButtonFX/src/sample/Controller.java
@@ -11,12 +11,7 @@ public class Controller {
     private Button clickMeButton;
 
     public void initialize() {
-        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("You clicked me!");
-            }
-        });
+        clickMeButton.setOnAction(event -> System.out.println("You clicked me!"));
     }
 }
 
