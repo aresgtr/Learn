ed291451caae063dcbee65f31271a5a1426863cf
aresgtr <784359442@qq.com>
2019-11-04 11:30:14 -0800

    Chaining java.util.funciton Fuctions

--- JAVA/C296-FunctionalInterfacesAndPredicates/src/com/company/Main.java
+++ JAVA/C296-FunctionalInterfacesAndPredicates/src/com/company/Main.java
@@ -3,10 +3,17 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+
+import org.w3c.dom.ls.LSOutput;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Main {
 
@@ -117,7 +124,7 @@ public class Main {
         };
 //        String firstName = getFirstName.apply(employees.get(1));
 //        System.out.println(firstName);
-        
+
         Random random1 = new Random();
         for (Employee employee: employees) {
             if (random1.nextBoolean()) {
@@ -126,6 +133,32 @@ public class Main {
                 System.out.println(getAName(getLastName, employee));
             }
         }
+
+        System.out.println("\n\n\n");
+
+        /*
+         * This part is for C299. Chaining java.util.function Functions
+         */
+
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> firstName = name -> name.substring(0, name.indexOf(' '));
+        Function chainedFunction = upperCase.andThen(firstName);
+
+        System.out.println(chainedFunction.apply(employees.get(0)));
+
+        BiFunction<String, Employee, String> concatAge = (String name, Employee employee) -> {
+            return name.concat(" " + employee.getAge());
+        };
+
+        String upperName = upperCase.apply(employees.get(0));
+        System.out.println(concatAge.apply(upperName, employees.get(0)));
+
+        IntUnaryOperator incBy5 = i -> i + 5;
+        System.out.println(incBy5.applyAsInt(10));
+
+        Consumer<String> c1 = s -> s.toUpperCase();
+        Consumer<String> c2 = s -> System.out.println(s);
+        c1.andThen(c2).accept("Hello, World!"); //  c1 lost the return value, since it does not return anything
     }
 
 
