6211930f034bb7938046bad1167fffc01e462ff4
aresgtr <784359442@qq.com>
2019-11-04 15:41:31 -0800

    Introduction to Unit Testing with JUnit

---
+++ JAVA/C317-Banking/src/com/company/BankAccountTest.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail("This test has yet to be implemented");
+    }
+
+}
