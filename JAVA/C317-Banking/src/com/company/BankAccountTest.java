95b8246bd931643f422a13d7dd7b08c53c1f0b8a
aresgtr <784359442@qq.com>
2019-11-04 16:38:28 -0800

    More Asserts (Optimized for JUnit 5)

--- JAVA/C317-Banking/src/com/company/BankAccountTest.java
+++ JAVA/C317-Banking/src/com/company/BankAccountTest.java
@@ -1,42 +1,74 @@
 package com.company;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Assertions;
+
 class BankAccountTest {
+
+    private BankAccount account;
+    private static int count;
+
+    @org.junit.jupiter.api.BeforeAll
+    static void beforeClass() {
+        System.out.println("This executes before any test cases. Count = " + count++);
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    void setup() {
+        account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
 
     @org.junit.jupiter.api.Test
     void deposit() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
     }
 
     @org.junit.jupiter.api.Test
-    void withdraw() {
-        fail("This test has yet to be implemented");
+    void withdraw_branch() {
+        double balance = account.withdraw(200.00, true);
+        assertEquals(800.00, balance, 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw_notBranch() {
+//        assertEquals(400.00, balance, 0);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            double balance = account.withdraw(600.00, false);
+        });
     }
 
     @org.junit.jupiter.api.Test
     void getBalance_deposit() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.jupiter.api.Test
     void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
 
     @org.junit.jupiter.api.Test
     void isChecking_true() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
 //        assertEquals(true, account.isChecking());
         assertTrue(account.isChecking());
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    static void afterClass() {
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void teardown() {
+        System.out.println("Count = " + count++);
     }
 
 
