fe2065eabd2c42704b0a8bbe598dae2a6bb87dbf
aresgtr <784359442@qq.com>
2019-11-04 16:10:08 -0800

    Asserts in Junit

--- JAVA/C317-Banking/src/com/company/BankAccountTest.java
+++ JAVA/C317-Banking/src/com/company/BankAccountTest.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
     }
 
     @org.junit.jupiter.api.Test
@@ -15,8 +19,30 @@ class BankAccountTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail("This test has yet to be implemented");
+    void getBalance_deposit() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+//        assertEquals(true, account.isChecking());
+        assertTrue(account.isChecking());
+    }
+
+
+//    @org.junit.jupiter.api.Test
+//    public void dummyTest() {
+//        assertEquals(20, 21);
+//    }
 
 }
