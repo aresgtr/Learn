afcef2941b83250791ba700594dac2d7b4af9ae6
aresgtr <784359442@qq.com>
2020-01-03 09:19:53 -0800

    stream

---
+++ MyJavaPrograms/Functional Interface Practice/src/io/github/aresgtr/StreamClass.java
@@ -0,0 +1,47 @@
+package io.github.aresgtr;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @URL  https://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
+ * @ToDo flatMap
+ */
+
+public class StreamClass {
+
+    public static void main(String[] args) {
+
+        List<String> legoSets = Arrays.asList(
+                "Palace Cinema",
+                "Parisian Restaurant",
+                "Detective Office",
+                "Brick Bank",
+                "Town Square",
+                "Downtown Diner"
+        );
+
+        legoSets
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("P"))
+                .sorted()
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        Stream.of("A", "B", "C")
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        System.out.println();
+
+        legoSets
+                .stream()
+                .map(a -> a + " is a good Lego Set")
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::println);
+    }
+}
