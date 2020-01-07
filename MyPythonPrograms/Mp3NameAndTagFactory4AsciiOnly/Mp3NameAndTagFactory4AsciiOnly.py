dc12ac9ccd0bb4035b456709dd0143432da139ef
aresgtr <784359442@qq.com>
2020-01-07 13:54:45 -0800

    Add method for reading file names into a list

--- MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/Mp3NameAndTagFactory4AsciiOnly.py
+++ MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/Mp3NameAndTagFactory4AsciiOnly.py
@@ -1,10 +1,15 @@
 from PIL import Image, ImageDraw
+import os
+
+inputPath = 'FactoryIn'
 
 def main():
     img = drawBlackEmpty()
     writeTextOnImage(img, "Hello")
     saveImage(img, 'hello')
 
+    fileNames = readFileNames(inputPath)
+    print(fileNames)
 
 def drawBlackEmpty():
     return Image.new('RGB', (600, 600), color = 'black')
@@ -12,9 +17,11 @@ def drawBlackEmpty():
 def writeTextOnImage(img, text):
     ImageDraw.Draw(img).text((100, 100), text, fill = (255, 255, 0))
 
-
 def saveImage(img, filename):
     img.save(filename + '.jpg')
+
+def readFileNames(path):
+    return os.listdir(path)
 
 if __name__ == '__main__':
     main()
