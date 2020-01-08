deea9d21e3235ef28c8aa360137a8d690897c1ff
aresgtr <784359442@qq.com>
2020-01-08 11:09:52 -0800

    More examples

--- MyJavaPrograms/RxJava响应式编程/src/main/java/Chapter2.java
+++ MyJavaPrograms/RxJava响应式编程/src/main/java/Chapter2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import rx.Observable;
 import rx.functions.Action1;
 import rx.functions.Func0;
@@ -33,8 +37,35 @@ public class Chapter2 {
             }
         });
 
+
+        //  From
+        FromArray().subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                System.out.println("FromArray: " + integer);
+            }
+        });
+
+        FromIterable().subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                System.out.println("From Iterable:" + integer);
+            }
+        });
+
+        //  buffer
+        bufferObserver().subscribe(new Action1<List<Integer>>() {
+            @Override
+            public void call(List<Integer> integers) {
+                System.out.println("bufferTime:" + integers);
+            }
+        });
+
+
+
     }
 
+    //  Just and Defer
     private static Observable<Long> getJust() {
         return Observable.just(System.currentTimeMillis());
     }
@@ -46,5 +77,22 @@ public class Chapter2 {
                 return getJust();
             }
         });
+    }
+
+    //  From (array or list both ok)
+    static Integer[] array = {0, 1, 2, 3, 4, 5};
+    private static Observable<Integer> FromArray() {
+        return Observable.from(array);
+    }
+
+    static List<Integer> list = new ArrayList<>(Arrays.asList(array));
+    private static Observable<Integer> FromIterable() {
+        return Observable.from(list);
+    }
+
+    //  buffer
+    private static Observable<List<Integer>> bufferObserver() {
+        return Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                .buffer(2, 3);
     }
 }
