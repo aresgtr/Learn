e48128553b3ca45de51f5268e9106e1d1227f59c
aresgtr <784359442@qq.com>
2020-01-07 21:29:50 -0800

    Add function to write image to mp3

--- MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/Mp3NameAndTagFactory4AsciiOnly.py
+++ MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/Mp3NameAndTagFactory4AsciiOnly.py
@@ -1,17 +1,23 @@
-from PIL import Image, ImageDraw, ImageFont
 import os
 
-inputPath = 'FactoryIn'
+import eyed3
+from PIL import Image, ImageDraw, ImageFont
 
 
 def main():
     img = drawBlackEmpty()
-    text = u"编程 - ひらがな - 히라가나"
+    text = u"你好 - ひらがな - 히라가나"
     writeTextOnImage(img, text)
     saveImage(img, 'hello')
+    inputPath = 'FactoryIn'
+    imageCachePath = 'ImageCache'
 
     fileNames = readFileNames(inputPath)
-    print(fileNames)
+    for name in fileNames:
+        audiofile = eyed3.load(inputPath + '\\' + name)
+        imagedata = open("hello.jpg", "rb").read()
+        audiofile.tag.images.set(3, imagedata, "image/jpeg", u"you can put a description here")
+        audiofile.tag.save()
 
 
 def drawBlackEmpty():
@@ -20,7 +26,10 @@ def drawBlackEmpty():
 
 def writeTextOnImage(img, text):
     font_size = 35
-    unicode_font = ImageFont.truetype("wqy-microhei.ttc", font_size)
+    #   for linux
+    # unicode_font = ImageFont.truetype("wqy-microhei.ttc", font_size)
+    #   目前不支持韩文
+    unicode_font = ImageFont.truetype("STKAITI.TTF", font_size)
     ImageDraw.Draw(img).text((100, 100), text, font=unicode_font)
 
 
