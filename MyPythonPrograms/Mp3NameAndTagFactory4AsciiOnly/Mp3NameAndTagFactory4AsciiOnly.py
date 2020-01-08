3cdfdd845461bef0ef110638333d5dcb2fecc357
aresgtr <784359442@qq.com>
2020-01-08 10:00:24 -0800

    Function for find number of lines

--- MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/Mp3NameAndTagFactory4AsciiOnly.py
+++ MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/Mp3NameAndTagFactory4AsciiOnly.py
@@ -3,34 +3,52 @@ import os
 import eyed3
 from PIL import Image, ImageDraw, ImageFont
 
+# Global Variables
+input_path = 'FactoryIn'
+image_cache_path = 'ImageCache'
+middle = 250
+max_char_len_per_line = 12
+
 
 def main():
-    img = drawBlackEmpty()
-    text = u"你好 - ひらがな - 히라가나"
-    writeTextOnImage(img, text)
-    saveImage(img, 'hello')
-    inputPath = 'FactoryIn'
-    imageCachePath = 'ImageCache'
 
-    fileNames = readFileNames(inputPath)
-    for name in fileNames:
-        audiofile = eyed3.load(inputPath + '\\' + name)
+    img = drawBlackEmpty()
+
+    # text = u"你好 - ひらがな - 히라가나"
+    text = u"你好你好你好你好你好你好你好"
+
+    num_of_lines = findNumberOfLines(text)
+    print(num_of_lines)
+
+    writeTextOnImage(img, text, middle)
+    saveImage(img, 'hello')
+
+    file_names = readFileNames(input_path)
+    for name in file_names:
+        audiofile = eyed3.load(input_path + '\\' + name)
         imagedata = open("hello.jpg", "rb").read()
         audiofile.tag.images.set(3, imagedata, "image/jpeg", u"you can put a description here")
         audiofile.tag.save()
+
+
+def findNumberOfLines(text):
+    num_of_lines = int(len(text) / max_char_len_per_line)
+    if len(text) % max_char_len_per_line:
+        num_of_lines = num_of_lines + 1
+    return num_of_lines
 
 
 def drawBlackEmpty():
     return Image.new('RGB', (600, 600), color='black')
 
 
-def writeTextOnImage(img, text):
+def writeTextOnImage(img, text, start_position):
     font_size = 35
     #   for linux
-    # unicode_font = ImageFont.truetype("wqy-microhei.ttc", font_size)
+    unicode_font = ImageFont.truetype("wqy-microhei.ttc", font_size)
     #   目前不支持韩文
-    unicode_font = ImageFont.truetype("STKAITI.TTF", font_size)
-    ImageDraw.Draw(img).text((100, 100), text, font=unicode_font)
+    # unicode_font = ImageFont.truetype("STKAITI.TTF", font_size)
+    ImageDraw.Draw(img).text((100, start_position), text, font=unicode_font)
 
 
 def saveImage(img, filename):
