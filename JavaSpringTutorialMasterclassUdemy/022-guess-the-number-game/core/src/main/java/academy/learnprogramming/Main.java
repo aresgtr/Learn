aebcf1f206f4e33c43c75977d12e41c36c069c77
Qi <784359442@qq.com>
2020-02-02 16:38:46 -0800

    Update multi module pom

---
+++ JavaSpringTutorialMasterclassUdemy/022-guess-the-number-game/core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,16 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by jasonsmac on 2020-02-02.
+ */
+public class Main {
+
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess the number game");
+    }
+}
