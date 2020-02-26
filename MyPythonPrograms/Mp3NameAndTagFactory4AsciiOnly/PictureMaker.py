6a92a2998d2657cebdda4a7951809f71a45efbb4
Qi <784359442@qq.com>
2020-02-25 22:37:26 -0800

    wip

--- MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/PictureMaker.py
+++ MyPythonPrograms/Mp3NameAndTagFactory4AsciiOnly/PictureMaker.py
@@ -5,25 +5,28 @@ input_path = 'Factory'
 image_cache_path = 'ImageCache'
 pixel = 600
 middle = pixel / 2
-margin = 60
-max_char_len_per_line = 7
-font_size = 80
-line_space = 40
+margin = 0
+max_char_len_per_line = 5
+font_size = 120
+
+unicode_font = ImageFont.truetype("msyhbd.ttc", font_size)
 
 
 def main():
     img = drawBlackEmpty()
 
+    #   TODO: for testing, will be removed
+    artist = '目前'
+    song_name = '目前不支持韩文目前不支持韩文'
+    text = '目前 - 目前不支持韩文目前不支持韩文'
+
+    if not is_ascii(song_name):
+        writeTextOnImage(img, text, artist, song_name)
+        saveImage(img, song_name)
+
 
 def drawBlackEmpty():
     return Image.new('RGB', (600, 600), color='black')
-
-    name = 'hihi'
-    song_name = 'hihihi'
-
-    if is_ascii(song_title) == False:
-        writeTextOnImage(img, pure_name, artist, song_title)
-        saveImage(img, pure_name)
 
 
 def is_ascii(s):
@@ -32,57 +35,62 @@ def is_ascii(s):
 
 def writeTextOnImage(img, text, artist, song_title):
     num_of_lines = findNumberOfLines(text)
-
-    #   for linux
-    # unicode_font = ImageFont.truetype("wqy-microhei.ttc", font_size)
-    #   目前不支持韩文
-    unicode_font = ImageFont.truetype("STKAITI.TTF", font_size)
+    line_space = findLineSpace(num_of_lines)
 
     if num_of_lines == 1:
         start_position = middle - font_size / 2
         ImageDraw.Draw(img).text((margin, start_position), text, font=unicode_font)
     elif num_of_lines == 2:
         start_position = middle - font_size - line_space / 2
-        ImageDraw.Draw(img).text((margin, start_position), artist + ' -', font=unicode_font)
+        ImageDraw.Draw(img).text((margin, start_position), artist, font=unicode_font)
         ImageDraw.Draw(img).text((margin, start_position + font_size + line_space), song_title, font=unicode_font)
     elif num_of_lines == 3:
         start_position = middle - font_size / 2 - line_space - font_size
-        ImageDraw.Draw(img).text((margin, start_position), artist + ' -', font=unicode_font)
-        if len(song_title) == max_char_len_per_line:  # one char out
+        ImageDraw.Draw(img).text((margin, start_position), artist, font=unicode_font)
+        if len(song_title) == max_char_len_per_line + 1:  # one char out
             song_title_1 = song_title[:-2]
             song_title_2 = song_title[-2:]
         else:
-            song_title_1 = song_title[:max_char_len_per_line - 1]
-            song_title_2 = song_title[max_char_len_per_line - 1:]
+            song_title_1 = song_title[:max_char_len_per_line]
+            song_title_2 = song_title[max_char_len_per_line:]
         ImageDraw.Draw(img).text((margin, start_position + font_size + line_space), song_title_1, font=unicode_font)
         ImageDraw.Draw(img).text((margin, start_position + font_size + line_space + font_size + line_space),
                                  song_title_2, font=unicode_font)
 
-    elif num_of_lines == 4:
+    else:  # 4 lines is max
         start_position = middle - font_size - line_space / 2 - line_space - font_size
-        ImageDraw.Draw(img).text((margin, start_position), artist + ' -', font=unicode_font)
+        ImageDraw.Draw(img).text((margin, start_position), artist, font=unicode_font)
 
-        song_title_1 = song_title[:max_char_len_per_line - 1]
-        song_title_2 = song_title[max_char_len_per_line - 1:(2 * max_char_len_per_line - 2)]
-        song_title_3 = song_title[(2 * max_char_len_per_line - 2):]
+        song_title_1 = song_title[:max_char_len_per_line]
+        song_title_2 = song_title[max_char_len_per_line:(2 * max_char_len_per_line)]
+        song_title_3 = song_title[(2 * max_char_len_per_line):]
 
         ImageDraw.Draw(img).text((margin, start_position + font_size + line_space), song_title_1, font=unicode_font)
         ImageDraw.Draw(img).text((margin, start_position + 2 * font_size + 2 * line_space), song_title_2,
                                  font=unicode_font)
-        ImageDraw.Draw(img).text((margin, start_position + 3 * font_size + 3 * line_space), song_title_2,
+        ImageDraw.Draw(img).text((margin, start_position + 3 * font_size + 3 * line_space), song_title_3,
                                  font=unicode_font)
 
 
 def findNumberOfLines(text):
     song_title_len = len(str(text.split(' - ')[1:])[2:-2])
+    artist_len = len(str(text.split(' - ')[:1])[2:-2])
 
-    if (len(text) - 1) <= max_char_len_per_line:
+    if (song_title_len + artist_len) < max_char_len_per_line:
         return 1
     else:
-        number_of_lines = song_title_len / (max_char_len_per_line - 1)
-        if song_title_len % (max_char_len_per_line - 1):
+        number_of_lines = song_title_len / max_char_len_per_line
+        if song_title_len % max_char_len_per_line:
             number_of_lines = number_of_lines + 1
         return int(number_of_lines + 1)
+
+
+def findLineSpace(num_of_lines):
+    return 25 + 10 * (4 - num_of_lines)
+
+
+def saveImage(img, filename):
+    img.save(filename + '.jpg')
 
 
 if __name__ == '__main__':
