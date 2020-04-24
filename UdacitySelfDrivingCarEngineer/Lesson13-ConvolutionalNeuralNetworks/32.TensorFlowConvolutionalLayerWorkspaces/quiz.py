8827985093e6919bdd0d4f6e27e82d63637c3f84
Qi <784359442@qq.com>
2020-04-24 12:03:36 -0700

    Update quiz.py

--- UdacitySelfDrivingCarEngineer/Lesson13-ConvolutionalNeuralNetworks/32.TensorFlowConvolutionalLayerWorkspaces/quiz.py
+++ UdacitySelfDrivingCarEngineer/Lesson13-ConvolutionalNeuralNetworks/32.TensorFlowConvolutionalLayerWorkspaces/quiz.py
@@ -21,10 +21,20 @@ def conv2d(input_array):
     # The shape of the filter bias is (output_depth,)
     # TODO: Define the filter weights `F_W` and filter bias `F_b`.
     # NOTE: Remember to wrap them in `tf.Variable`, they are trainable parameters after all.
-    F_W = tf.Variable(tf.truncated_normal((2, 2, 1, 3)))
+    F_W = tf.Variable(tf.truncated_normal((2, 2, 1, 3)))  # out_height, out_width, in_depth, out_depth
     F_b = tf.Variable(tf.zeros(3))
     # TODO: Set the stride for each dimension (batch_size, height, width, depth)
     strides = [1, 2, 2, 1]
+    """上面的怎么算出来的 (1,4,4,1) to (1,2,2,3)"""
+    """strides两边都是1，里面最好也是1"""
+    in_height = 4
+    in_width = 4
+    filter_height = 2
+    filter_width = 2
+    out_height = np.ceil(float(in_height - filter_height + 1) / float(strides[1]))
+    out_width = np.ceil(float(in_width - filter_width + 1) / float(strides[2]))
+    print(out_height)
+    print(out_width)
     # TODO: set the padding, either 'VALID' or 'SAME'.
     padding = 'VALID'
     # https://www.tensorflow.org/versions/r0.11/api_docs/python/nn.html#conv2d
