4fb0cb46493d1f34de1ed7a1f4bda855b0370b80
Qi <784359442@qq.com>
2020-04-26 18:38:38 -0700

    Update project

--- UdacitySelfDrivingCarEngineer/Lesson18Project-CarND-Behavioral-Cloning-P3-master/imagetemp.py
+++ UdacitySelfDrivingCarEngineer/Lesson18Project-CarND-Behavioral-Cloning-P3-master/imagetemp.py
@@ -1,8 +1,6 @@
 import csv
 import cv2
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from matplotlib import pyplot as plt
 
 """
 Import training data
@@ -27,18 +25,6 @@ for line in lines:
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
-
-X_train = np.array(images)
-y_train = np.array(measurements)
-
-"""
-Neural Network
-"""
-model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
-model.add(Dense(1))
-
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-
-model.save('model.h5')
+    plt.imshow(image)
+    plt.show()
+    break
