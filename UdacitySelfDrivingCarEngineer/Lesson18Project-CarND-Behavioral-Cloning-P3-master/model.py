2eb963aa588975df0a8a1df0c5992f6f3925a46a
Qi <784359442@qq.com>
2020-04-26 22:17:00 -0700

    Update with generator

--- UdacitySelfDrivingCarEngineer/Lesson18Project-CarND-Behavioral-Cloning-P3-master/model.py
+++ UdacitySelfDrivingCarEngineer/Lesson18Project-CarND-Behavioral-Cloning-P3-master/model.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 
 """
@@ -20,25 +22,41 @@ with open(csv_location) as csvfile:
     for line in reader:
         lines.append(line)
 
-images = []
-measurements = []
-# dsize = (160, 80)
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = image_dir + filename
-    image = cv2.imread(current_path)
-    # image = cv2.resize(image, dsize)
-    image_flipped = np.fliplr(image)
-    images.append(image)
-    images.append(image_flipped)
-    measurement = float(line[3])
-    measurement_flipped = -measurement
-    measurements.append(measurement)
-    measurements.append(measurement_flipped)
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:  # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset + batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                source_path = batch_sample[0]
+                filename = source_path.split('/')[-1]
+                current_path = image_dir + filename
+                center_image = cv2.imread(current_path)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield shuffle(X_train, y_train)
+
+
+# Set our batch size
+batch_size = 32
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
+
+# ch, row, col = 3, 160, 320  # Trimmed image format
 
 """
 Neural Network
@@ -46,7 +64,7 @@ Neural Network
 model = Sequential()
 
 # Normalize Data
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
 
 # Crop off unused
 model.add(Cropping2D(cropping=((70, 25), (10, 10))))
@@ -70,6 +88,8 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples), nb_epoch=5, verbose=1)
 
 model.save('model.h5')
---
+++ UdacitySelfDrivingCarEngineer/Lesson18Project-CarND-Behavioral-Cloning-P3-master/model_old.py
@@ -0,0 +1,109 @@
+import csv
+import cv2
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D, Dropout
+import matplotlib.pyplot as plt
+
+"""
+Import training data
+"""
+csv_location = 'D:\\UdacityCarData\\data\\driving_log.csv'
+# csv_location = '/opt/carnd_p3/data/driving_log.csv'
+image_dir = 'D:\\UdacityCarData\\data\\IMG\\'
+# image_dir = '/opt/carnd_p3/data/IMG/'
+
+lines = []
+with open(csv_location) as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # skip first line
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = image_dir + filename
+    image = cv2.imread(current_path)
+    image_flipped = np.fliplr(image)
+    images.append(image)
+    images.append(image_flipped)
+    measurement = float(line[3])
+    measurement_flipped = -measurement
+    measurements.append(measurement)
+    measurements.append(measurement_flipped)
+
+X_train_0 = np.array(images)
+y_train_0 = np.array(measurements)
+
+# For my own data
+csv_location = 'D:\\UdacityCarData\\my_own_data\\driving_log.csv'
+# csv_location = '/opt/carnd_p3/data/driving_log.csv'
+image_dir = 'D:\\UdacityCarData\\my_own_data\\IMG\\'
+# image_dir = '/opt/carnd_p3/data/IMG/'
+
+lines = []
+with open(csv_location) as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # skip first line
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    filename = filename.split('\\')[-1]
+    current_path = image_dir + filename
+    image = cv2.imread(current_path)
+    image_flipped = np.fliplr(image)
+    images.append(image)
+    images.append(image_flipped)
+    measurement = float(line[3])
+    measurement_flipped = -measurement
+    measurements.append(measurement)
+    measurements.append(measurement_flipped)
+
+X_train_1 = np.array(images)
+y_train_1 = np.array(measurements)
+
+X_train = np.append(X_train_0, X_train_1)
+y_train = np.append(y_train_0, y_train_1)
+
+print(len(y_train))
+"""
+Neural Network
+"""
+model = Sequential()
+
+# Normalize Data
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+
+# Crop off unused
+model.add(Cropping2D(cropping=((70, 25), (10, 10))))
+
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+
+# Dropout layer
+model.add(Dropout(0.5, noise_shape=None, seed=None))
+
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
