536a72258ac8d6ecf79f206d10958052472898c0
Qi <784359442@qq.com>
2020-04-30 18:20:05 -0700

    update

---
+++ UdacitySelfDrivingCarEngineer/T2-Lesson2-MarkovLocalization/35.GetPseudoRanges/35.GetPseudoRanges/helpers.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#ifndef HELP_FUNCTIONS_H
+#define HELP_FUNCTIONS_H
+
+#include <math.h>
+
+class Helpers {
+public:
+    // definition of one over square root of 2*pi:
+    //constexpr static float STATIC_ONE_OVER_SQRT_2PI = 1 / sqrt(2 * M_PI);
+    constexpr static float STATIC_ONE_OVER_SQRT_2PI = 0.398942280401432677939946059934381868475858631164934657665;
+
+    /**
+     * normpdf(X,mu,sigma) computes the probability function at values x using the
+     * normal distribution with mean mu and standard deviation std. x, mu and
+     * sigma must be scalar! The parameter std must be positive.
+     * The normal pdf is y=f(x,mu,std)= 1/(std*sqrt(2pi)) e[ -(x−mu)^2 / 2*std^2 ]
+     */
+    static float normpdf(float x, float mu, float std) {
+        return (STATIC_ONE_OVER_SQRT_2PI / std) * exp(-0.5 * pow((x - mu) / std, 2));
+    }
+};
+
+#endif  // HELP_FUNCTIONS_H
