05f08b242a84ca6aab1a87b3a905735f6b75fb42
Qi <784359442@qq.com>
2020-05-01 13:25:03 -0700

    update

---
+++ UdacitySelfDrivingCarEngineer/T2-Lesson5-ImplementationOfAParticleFilter/20.ParticleWeightsSolution/20.ParticleWeightsSolution/multiv_gauss.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef MULTIV_GAUSS_H
+#define MULTIV_GAUSS_H
+
+double multiv_prob(double sig_x, double sig_y, double x_obs, double y_obs,
+    double mu_x, double mu_y);
+
+#endif  // MULTIV_GAUSS_H
