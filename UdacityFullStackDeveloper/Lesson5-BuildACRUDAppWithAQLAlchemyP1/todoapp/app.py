b400b43603c3c23fd4c0070b70b404d35dd9eef5
aresgtr <784359442@qq.com>
2020-05-31 19:06:28 -0700

    Finish AJAX

--- UdacityFullStackDeveloper/Lesson5-BuildACRUDAppWithAQLAlchemyP1/todoapp/app.py
+++ UdacityFullStackDeveloper/Lesson5-BuildACRUDAppWithAQLAlchemyP1/todoapp/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+"""
+https://knowledge.udacity.com/questions/187503
+"""
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
@@ -14,7 +19,19 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+
+# db.create_all()
+
+
+@app.route('/todos/create', methods=['GET'])
+def create_todo():
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description': todo.description
+    })
 
 
 @app.route('/')
