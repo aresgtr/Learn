b335cd122af62df8022be91d48833fa97a1eb380
Qi <784359442@qq.com>
2020-06-03 17:58:10 -0700

    Sync with solution

--- UdacityFullStackDeveloper/Lesson5-BuildACRUDAppWithAQLAlchemyP1/todoapp/app.py
+++ UdacityFullStackDeveloper/Lesson5-BuildACRUDAppWithAQLAlchemyP1/todoapp/app.py
@@ -1,17 +1,12 @@
-import sys
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-"""
-https://knowledge.udacity.com/questions/187503
-"""
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 
@@ -19,42 +14,66 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
+    completed = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 
-@app.route('/todos/create', methods=['GET'])
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
+
+
+# note: more conventionally, we would write a
+# POST endpoint to /todos for the create endpoint:
+# @app.route('/todos', method=['POST'])
+@app.route('/todos/create', methods=['POST'])
 def create_todo():
-    # description = request.get_json()['description']
-    # todo = Todo(description=description)
-    # db.session.add(todo)
-    # db.session.commit()
-    # return jsonify({
-    #     'description': todo.description
-    # })
     error = False
-    # initialize the todo_object
-    todo_object = {}
+    body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        todo = Todo(description=description, completed=False)
         db.session.add(todo)
         db.session.commit()
-        # we have access to the todo, so we can save it to the todo_object here
-        todo_object['description'] = todo.description
+        body['id'] = todo.id
+        body['completed'] = todo.completed
+        body['description'] = todo.description
     except:
         error = True
         db.session.rollback()
-        print(sys.exc_info)
+        print(sys.exc_info())
     finally:
         db.session.close()
-    if not error:
-        # since todo_object already looks has the description key, we can jsonify the whole thing
-        return jsonify(todo_object)
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', todos=Todo.query.order_by('id').all())
