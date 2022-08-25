690cda1f5a4df6c181b4d38ac2b4419d47455a42
Qi <784359442@qq.com>
2022-08-25 11:48:21 +0800

    Update L36PatternMatching.scala

--- udemy-rock-the-jvm-scala-beginners/src/main/scala/lectures/part4pattern_matching/L36PatternMatching.scala
+++ udemy-rock-the-jvm-scala-beginners/src/main/scala/lectures/part4pattern_matching/L36PatternMatching.scala
@@ -30,5 +30,56 @@ object L36PatternMatching extends App {
   println(greeting) //>> Hi my name is Bob and I can't drink in the US
   // We can not only pattern match against any value, we can pattern match a case class pattern and extract values
 
+  /*
+    1. Cases are match in order
+    2. What if no cases match? MatchError
+    3. type of the pattern matching expression? Unified type of all the types in all the cases
+    4. Pattern Matching works really well with case classes*
+   */
 
+  // Pattern Matching on sealed hierarchies
+  sealed class Animal
+  case class Dog(breed: String) extends Animal
+  case class Parrot(greeting: String) extends Animal
+
+  val animal: Animal = Dog("Terra Nova")
+  animal match {
+    case Dog(someBreed) => println(s"Matched a dog of the $someBreed breed")
+  }
+  // The above code should not compile because the matching doesn't cover all case classes (however it compiles locally)
+  // If you remove sealed, it compiles. Sealed classes help you to check.
+
+  /*
+    Exercise
+    simple function uses Pattern Matching
+    takes an Expr => human readable form
+
+    Sum(Number(2), Number(3)) => 2 + 3
+    Sum(Number(2), Number(3), Number(4)) => 2 + 3 + 4
+    Prod(Sum(Number(2), Number(1)), Number(3)) => (2 + 1) * 3
+    Sum(Prod(Number(2), Number(1)), Number(3)) => 2 * 1 + 3
+   */
+  trait Expr
+  case class Number(n: Int) extends Expr
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+  case class Prod(e1: Expr, e2: Expr) extends Expr
+
+  def show(e: Expr): String = e match {
+    case Number(n) => s"$n"
+    case Sum(e1, e2) => show(e1) + " + " + show(e2)
+    case Prod(e1, e2) => {
+      def mayberShowParentheses(expr: Expr) = expr match {
+        case Prod(_, _) => show(expr)
+        case Number(_) => show(expr)
+        case _ => "(" + show(expr) + ")"
+      }
+
+      mayberShowParentheses(e1) + " * " + mayberShowParentheses(e2)
+    }
+  }
+
+  println(show(Sum(Number(2), Number(3))))                    //>> 2 + 3
+  println(show(Sum(Sum(Number(2), Number(3)), Number(4))))    //>> 2 + 3 + 4
+  println(show(Prod(Sum(Number(2), Number(1)), Number(3))))   //>> (2 + 1) * 3
+  println(show(Sum(Prod(Number(2), Number(1)), Number(3))))   //>> 2 * 1 + 3
 }
