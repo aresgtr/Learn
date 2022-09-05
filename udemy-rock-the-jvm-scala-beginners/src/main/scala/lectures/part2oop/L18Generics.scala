2d2e3bb708e512ebb42703e62260502b43ac3745
Qi <784359442@qq.com>
2022-09-05 14:11:03 +0800

    Update L18Generics.scala

--- udemy-rock-the-jvm-scala-beginners/src/main/scala/lectures/part2oop/L18Generics.scala
+++ udemy-rock-the-jvm-scala-beginners/src/main/scala/lectures/part2oop/L18Generics.scala
@@ -86,40 +86,27 @@ object L18Generics extends App {
      */
 
     def head: A
-
     def tail: MyList[A]
-
     def isEmpty: Boolean
-
     def add[B >: A](element: B): MyList[B]
-
     def printElements: String
-
     // polymorphic call
     override def toString: String = "[" + printElements + "]"
   }
 
   object Empty extends MyList[Nothing] {
     override def head: Nothing = throw new NoSuchElementException // "Nothing" is a proper substitute for "Any" type
-
     override def tail: MyList[Nothing] = throw new NoSuchElementException
-
     override def isEmpty: Boolean = true
-
     override def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
-
     override def printElements: String = ""
   }
 
   class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     override def head: A = h
-
     override def tail: MyList[A] = t
-
     override def isEmpty: Boolean = false
-
     override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
-
     override def printElements: String =
       if (t.isEmpty) "" + h
       else "" + h + " " + t.printElements
