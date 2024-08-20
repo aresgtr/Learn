b5ccaacc3bac2787117a5772a5b6436c9d10b48a
Qi <784359442@qq.com>
2024-08-20 16:45:30 -0700

    Housekeeping and remove unnecessary files

--- JavaSpringTutorialMasterclassUdemy/014_hello-maven/src/main/java/academy/learnprogramming/HelloMaven.java
+++ JavaSpringTutorialMasterclassUdemy/014_hello-maven/src/main/java/academy/learnprogramming/HelloMaven.java
@@ -1,14 +1,8 @@
 package academy.learnprogramming;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class HelloMaven {
 
-    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
-
     public static void main(String[] args) {
-        log.info("Hello Info");
-        log.info("Hello Debug");
+        System.out.println("Hello Maven");
     }
 }
